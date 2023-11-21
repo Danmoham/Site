f567da99ab3a0df935815c782d8312532d5f3750
Danmoham <dmoham35@gmail.com>
2023-11-21 14:59:07 +0000

    done

---
+++ Js/index.js
@@ -0,0 +1,7 @@
+const toggleMenu = () =>{
+    const myMenu = document.querySelector(".menu-links")
+    const myIcon = document.querySelector(".hamburger-icon")
+    myMenu.classList.toggle("open")
+    myIcon.classList.toggle("open")
+
+}
